77f14447bd479692c234680e57426fae3f96ca46
rob737 <robinsrivastava737@gmail.com>
2021-05-02 20:55:13 +0530

    setting up structure

---
+++ src/main/java/com/verticalScale/myGroceryList/MyGroceryListApplication.java
@@ -0,0 +1,13 @@
+package com.verticalScale.myGroceryList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGroceryListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGroceryListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verticalScale/myGroceryList/MyGroceryListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verticalScale.myGroceryList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGroceryListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
