9d8493250ba71c9097c4468af4f63c1ee6411d27
rob737 <robinsrivastava737@gmail.com>
2021-05-03 21:26:42 +0530

    created model classes

--- src/main/java/com/verticalScale/myGroceryList/model/DairyProduct.java
+++ src/main/java/com/verticalScale/myGroceryList/model/DairyProduct.java
@@ -1,7 +1,17 @@
 package com.verticalScale.myGroceryList.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class DairyProduct {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private double price;
+    private double nutritionValue;
+    private double monthlyQuota;
+    private int userId;
 }
--- src/main/java/com/verticalScale/myGroceryList/model/User.java
+++ src/main/java/com/verticalScale/myGroceryList/model/User.java
@@ -5,14 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Vegetable {
+public class User {
     @Id
     @GeneratedValue
-    private int id;
-    private String name;
-    private double price;
-    private double nutritionValue;
-    private double monthlyQuota;
     private int userId;
-
 }
